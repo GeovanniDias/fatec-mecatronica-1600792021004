2c6b24805acdf602b9891a2d8187973dda99f1c5
GeovanniDias <70581682+GeovanniDias@users.noreply.github.com>
2020-09-15 08:33:30 -0300

    Create ltpc1-aula04.py

---
+++ ltpc1-aula04.py
@@ -0,0 +1,8 @@
+#pede ao usuario para digitar um valor para a idade
+idade = int(input("informe sua idade:"))
+#pergunta, no código, se a idade for maior ou igual a 18
+if idade >= 18:
+   #este código pertence ao bloco do if
+   #print("você possui 18 anos ou mais")
+   #print ("Agora! Você pode ver todas as páginas de jogos da 'Steam")
+   print("Final do programa")
